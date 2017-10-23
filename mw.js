bfe446c70f4504f07207621d409dbd33371232b7
RomeDorado <romedorado@gmail.com>
2017-10-23 15:26:51 +0800

    location

--- mw.js
+++ mw.js
@@ -1,6 +1,6 @@
 module.exports = {
     logIncomingMessage: function (event, next) {
-        console.log(JSON.stringify(event.geo));          
+        console.log(JSON.stringify(event.sourceEvent.geo));          
         next();      
     }
 }
