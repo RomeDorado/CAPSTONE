981e675e8a859d42c4aa15a1c37aa4efe05185c5
RomeDorado <romedorado@gmail.com>
2018-01-06 19:11:05 +0800

    trigger

--- app.js
+++ app.js
@@ -19,75 +19,14 @@ const bot = new builder.UniversalBot(connector);
 //=========================================================
 bot.use(builder.Middleware.dialogVersion({ version: 1.0, resetCommand: /^reset/i }));
 bot.use(builder.Middleware.sendTyping());
-bot.use({
-    botbuilder: (session, next) => {
-        var startOver = /^started|get started|start over|get started|hi|hello|Get_Started/i.test(session.message.text);
-
-        if (startOver) {
-            session.userData = {};
-        }
-        if (!session.userData.firstRun) {
-            session.userData.firstRun = true;
-            session.beginDialog('/GetStarted');
-        } else { next(); }
-    }
-});
-
 //=========================================================
 // Bot's Dialogs
 //=========================================================
 
 bot.dialog('/', dialogs.default);
-bot.dialog('/GetStarted', dialogs.getStarted);
-bot.dialog('/Menu', dialogs.menu);
+bot.dialog('/GetStarted', dialogs.getStarted).triggerAction({matches:/^started|get started|start over|get started|hi|hello|Get_Started/i});
+bot.dialog('/Menu', dialogs.menu).triggerAction({matches:/^menu|main menu|Main menu/i});
 bot.dialog('/Subscribe', dialogs.subscribe);
-
-
-
-//CB below
-// bot.dialog('/CreditCards', dialogs.creditCard.main);
-// bot.dialog('/CreditCards/Requirements/CardOptions', dialogs.creditCard.main_1);
-// bot.dialog('/CreditCards/Rewards', dialogs.creditCard.rewards);
-// bot.dialog('/CardOptions/Rewards', dialogs.creditCard.rewards_1);
-// bot.dialog('/CreditCards/CashBack', dialogs.creditCard.cashBack);
-// bot.dialog('/CardOptions/CashBack', dialogs.creditCard.cashBack_1);
-// bot.dialog('/CreditCards/Premier', dialogs.creditCard.premier);
-// bot.dialog('/CardOptions/Premier', dialogs.creditCard.premier_1);
-// bot.dialog('/CreditCards/Shell', dialogs.creditCard.shell);
-// bot.dialog('/CardOptions/Shell', dialogs.creditCard.shell_1);
-// bot.dialog('/CreditCards/Mercury', dialogs.creditCard.mercury);
-// bot.dialog('/CardOptions/Mercury', dialogs.creditCard.mercury_1);
-// bot.dialog('/CreditCards/Requirements', dialogs.creditCard.requirements);
-// bot.dialog('/CreditCards/Requirements/InstantApproval', dialogs.creditCard.instant_approval);
-// bot.dialog('/InstantApproval', dialogs.creditCard.instant_approval_1);
-// bot.dialog('/Requirements', dialogs.creditCard.requirements_1);
-// bot.dialog('/GetDetails', dialogs.getDetails.main);
-// bot.dialog('/GetDetails/Email', dialogs.getDetails.email);
-// bot.dialog('/GetDetails/Loans', dialogs.getDetails.loans);
-// bot.dialog('/Loans', dialogs.loans.main);
-// bot.dialog('/Loans/Requirements', dialogs.loans.loanRequirements);
-// bot.dialog('/Loans/Info', dialogs.loans.loanInfo);
-// bot.dialog('/Loans/Decline', dialogs.loans.loanDecline);
-// bot.dialog('/Loans/selfEmployed', dialogs.loans.selfEmployed);
-// bot.dialog('/Loans/Proceed', dialogs.loans.proceed);
-// bot.dialog('/Loans/Accept', dialogs.loans.loanAccept);
-// bot.dialog('/UsageDeals', dialogs.usageDeals.main);
-// bot.dialog('/UsageDeals/Dining', dialogs.usageDeals.dining);
-// bot.dialog('/UsageDeals/Travel', dialogs.usageDeals.travel);
-// bot.dialog('/UsageDeals/Online', dialogs.usageDeals.online).triggerAction({matches:/online/i});;
-// bot.dialog('/UsageDeals/DiningMoreShakeys', dialogs.usageDeals.diningMoreShakeys);
-// bot.dialog('/UsageDeals/DiningMapShakeys', dialogs.usageDeals.diningMapShakeys);
-// bot.dialog('/UsageDeals/ReserveShakeys', dialogs.usageDeals.diningReserveShakeys);
-// bot.dialog('/UsageDeals/TravelMoreFairmont', dialogs.usageDeals.travelMoreFairmont);
-// bot.dialog('/UsageDeals/TravelMoreEmirates', dialogs.usageDeals.travelMoreEmirates);
-// bot.dialog('/UsageDeals/DiningMoreNanbantei', dialogs.usageDeals.diningMoreNanbantei);
-// bot.dialog('/UsageDeals/DiningMapNanbantei', dialogs.usageDeals.diningMapNanbantei);
-// bot.dialog('/UsageDeals/ReserveNanbantei', dialogs.usageDeals.diningReserveNanbantei);
-
-// bot.dialog('/UsageDeals/Installments', dialogs.usageDeals.main);
-// bot.dialog('/UsageDeals/Bill', dialogs.usageDeals.main);
-// bot.dialog('/UsageDeals/World', dialogs.usageDeals.main);
-
 
 //=========================================================
 // Server Setup
