928b8e3a6affc8f774facc0d1e13e7c77542db63
RomeDorado <romedorado@gmail.com>
2018-01-08 18:22:29 +0800

    wit

--- src/dialogs/default.js
+++ src/dialogs/default.js
@@ -4,6 +4,7 @@ const api = require('../helpers/apiRequest');
 const consts = require('../helpers/consts');
 const card = require('../helpers/cardBuilder');
 const WIT_TOKEN = "OAC2GWS2OVEXUJV5TUQX5FIDJ2F466EH"
+const Wit = require('node-wit');
 module.exports = 
 [
     (session, args, next) => {
