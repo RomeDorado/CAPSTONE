dc775e192a0177aea58064e28e934aadf84c87b9
RomeDorado <romedorado@gmail.com>
2018-01-12 00:27:46 +0800

    document status

--- src/dialogs/documentStatus.js
+++ src/dialogs/documentStatus.js
@@ -22,21 +22,22 @@ module.exports = [
         var domain = email.split("@");
 
         if(domain[1] == "ust-ics.mygbiz.com"){
-            api.mailBoxLayer(email, (err, res) => {
-                console.log(res, " res ");
-                if (!err) {
-                    if(res.smtp_check){
-                        var documentAccess = true;
-                        usersession.createUserIfUnique(session, undefined, documentAccess)
-                    }else{
-                        session.send(consts.prompts.INVALID_EMAIL);
-                        var cardName = card.getName(consts.menus.enter_email);
-                        var msg = card(session, consts.menus.enter_email, cardName);                    
-                        builder.Prompts.choice(session, msg, card.choices(consts.menus.enter_email), { maxRetries:0,promptAfterAction:false});
-                    }
-                }
+            console.log(email);
+            // api.mailBoxLayer(email, (err, res) => {
+            //     console.log(res, " res ");
+            //     if (!err) {
+            //         if(res.smtp_check){
+            //             var documentAccess = true;
+            //             usersession.createUserIfUnique(session, undefined, documentAccess)
+            //         }else{
+            //             session.send(consts.prompts.INVALID_EMAIL);
+            //             var cardName = card.getName(consts.menus.enter_email);
+            //             var msg = card(session, consts.menus.enter_email, cardName);                    
+            //             builder.Prompts.choice(session, msg, card.choices(consts.menus.enter_email), { maxRetries:0,promptAfterAction:false});
+            //         }
+            //     }
 
-            });
+            // });
         }else{
             console.log("wrong email format")
             session.send(consts.prompts.INVALID_EMAIL);
