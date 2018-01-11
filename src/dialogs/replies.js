e43333f0489cb1bcc31fd994ea05064d36c33321
RomeDorado <romedorado@gmail.com>
2018-01-11 22:19:07 +0800

    hard code

--- src/dialogs/replies.js
+++ src/dialogs/replies.js
@@ -18,9 +18,8 @@ module.exports = [
       
       request(options, function (error, response, body) {
         if (error) throw new Error(error);
-        var replies = body.data;
-        console.log(body);
-        console.log(body.data);
+        var replies = body;
+        console.log(replies.data);        
         var random = replies[Math.floor(Math.random() * replies.length)];
         session.send(random);
 
