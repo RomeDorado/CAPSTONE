97797a34f1330636e15a665e0d796aa9e823ef13
RomeDorado <romedorado@gmail.com>
2018-01-13 12:54:25 +0800

    built in nlp

--- src/dialogs/default.js
+++ src/dialogs/default.js
@@ -3,32 +3,49 @@ const format = require('string-format');
 const api = require('../helpers/apiRequest');
 const consts = require('../helpers/consts');
 const card = require('../helpers/cardBuilder');
-
+const WIT_TOKEN = "OAC2GWS2OVEXUJV5TUQX5FIDJ2F466EH"
+const {Wit, log} = require('node-wit');
 module.exports =
 [
     (session, args, next) => {
 
   console.log(session.message.user.name);
+  var entity = args || session.message.text;
+  const client = new Wit({accessToken: WIT_TOKEN});
 
-let entities = ('nlp' in session.message.sourceEvent.message) ? session.message.sourceEvent.message.nlp.entities : undefined;
-console.log(entities)
-        //If NLP is Entities present        
-        if(entities !== undefined){
-            var intent = Object.keys(entities).length != 0 && !(Object.keys(entities).length > 1) ? entities[Object.keys(entities)][0].value : 'default';
-            console.log(intent);
-        } else {
-            var intent = 'default';
-        }
-                getWitIntents(intent, session);  
+        client.message(entity, {})
+        .then((data) => {
+            var results = data;
+            var entities = results.entities;
+                console.log(JSON.stringify(entities));
+                if(entities.intent == (null || undefined)){
+                    //send tix?
+                }else{
+                    var intent = entities.intent[0].value;
+                }
+
+                console.log(intent);
+                if(('inquiry_type' in entities)){var inquiry_type = entities.inquiry_type[0].value;}
+                if(('emotion_type' in entities)){var emotion_type = entities.emotion_type[0].value;}
+                getWitIntents(intent, inquiry_type, emotion_type, session);
+
+        })
+        .catch(console.error)
+
     }
 ]
 
 
 
-function getWitIntents(intent, session){
+function getWitIntents(intent, inquiry_type, emotion_type, session){
     switch(intent){
 
-        case 'get_greetings':           
+        case 'get_greetings':
+            // let random = [ 'Hey! Welcome to IICS Bot! How may I help you?',
+            //                     'Heeyy!! What can I do for you today?',
+            //                   'Sup! What can I do for you today?'];
+            // let reply = random[Math.floor(Math.random() * random.length)];
+            // session.send(reply);
             session.replaceDialog('/Replies', intent);
         break;
 
@@ -98,10 +115,6 @@ function getWitIntents(intent, session){
 
                 case 'get_help':
                 //trigger tix or live chat
-                break;
-
-                case 'default':
-                session.replaceDialog('/Confusion');
                 break;
 
                 default:
