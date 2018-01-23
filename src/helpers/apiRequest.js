251ec8c37e5dc8eb53b5aa7b85953df101dec903
RomeDorado <romedorado@gmail.com>
2018-01-23 13:11:06 +0800

    log user convo

--- src/helpers/apiRequest.js
+++ src/helpers/apiRequest.js
@@ -89,7 +89,7 @@ module.exports.checkUser =
         request(options, function (error, response, body) {                    
         if (error) throw new Error(error);
         console.log(body, "checkuser")
-        callback(null, JSON.parse(body));
+        callback(null, body);
         });
 }
 
