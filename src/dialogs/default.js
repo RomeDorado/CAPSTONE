26e695ef6d707913734e914df80971bb65123773
James Neil Tucay <jamesneil2k@gmail.com>
2018-02-08 16:47:19 +0800

    get_inquiry_it_about_general

--- src/dialogs/default.js
+++ src/dialogs/default.js
@@ -69,6 +69,10 @@ function getWitIntents(intent, professor, time, session){
             session.replaceDialog('/Replies', intent);
         break;
 
+        case 'get_inquiry_it_about_general':
+        session.replaceDialog('/Replies', intent);
+        break;
+
         case 'get_farewell':
             session.send(' Just hit me up whenever you need me :)');
         break;
