2f379609e47177db3209925fdb4667f42e4842be
RomeDorado <romedorado@gmail.com>
2018-02-25 12:51:17 +0800

    sched

--- src/dialogs/facultySched.js
+++ src/dialogs/facultySched.js
@@ -25,6 +25,7 @@ module.exports.room = [
         try {
             console.log('went down here')
             api.room(session, args.firstname, args.prof, (err, results) => {
+                console.log(results, "asd")
                 // if (results.success) {
                     //dagdag kapag walang time si prof
                     session.endDialog(format(consts.prompts.PROF_ROOM, "at " + results.data));
@@ -33,7 +34,7 @@ module.exports.room = [
         }catch (exception) {
             api.room(session, "", args.prof, (err, results) => {
                 if (results.success) {//dagdag kapag walang time si prof
-                    console.log(results)
+                    console.log(results, "catch")
                     session.endDialog(format(consts.prompts.PROF_ROOM, "at " + results.data));
                 }
             })
