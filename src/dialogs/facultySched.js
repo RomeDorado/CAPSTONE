61ed517a9a6eb52e3571034008737f976c225013
RomeDorado <romedorado@gmail.com>
2018-02-25 11:44:53 +0800

    sched

--- src/dialogs/facultySched.js
+++ src/dialogs/facultySched.js
@@ -9,6 +9,7 @@ const usersession = require('../helpers/usersession');
 
 module.exports.nextClass = [
     (session, args) => {
+        if(!args.firstname) args.firstname = ""
         api.room(session, args.firstname, args.prof, (err, results) => {
             if(results.success){//dagdag kapag walang time si prof
                 session.endDialog(format(consts.prompts.GET_STARTED, results.data));
@@ -20,6 +21,7 @@ module.exports.nextClass = [
 
 module.exports.room = [
     (session, args) => {
+        if(!args.firstname) args.firstname = ""
         api.room(session, args.firstname, args.prof, (err, results) => {
             if(results.success){//dagdag kapag walang time si prof
                 session.endDialog(format(consts.prompts.GET_STARTED, "at " + results.data));
