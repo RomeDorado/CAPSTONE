6c6682745fcc2351b51b590cab50cd9598f5bdc8
RomeDorado <romedorado@gmail.com>
2018-02-25 21:50:06 +0800

    sched

--- src/dialogs/checkAccess.js
+++ src/dialogs/checkAccess.js
@@ -29,11 +29,11 @@ module.exports = [
             console.log(args, 'args check access')
             switch(args.intent){
                 case 'get_next_class':
-                    session.replaceDialog('/FacultySched/NextClass', {firstname: args.firstname, prof: args.prof, datetime: datetime});
+                    session.replaceDialog('/FacultySched/NextClass', {firstname: args.firstname, prof: args.prof, datetime: args.datetime});
                 break;
 
                 case 'get_faculty_room':
-                    session.replaceDialog('/FacultySched/Room', {firstname: args.firstname, prof: args.prof, datetime: datetime});
+                    session.replaceDialog('/FacultySched/Room', {firstname: args.firstname, prof: args.prof, datetime: args.datetime});
                 break;
             }            
             
