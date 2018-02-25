893798669ef7452ef12e680d5c1b3043db48251f
RomeDorado <romedorado@gmail.com>
2018-02-25 13:17:13 +0800

    sched

--- src/dialogs/facultySched.js
+++ src/dialogs/facultySched.js
@@ -9,29 +9,16 @@ const usersession = require('../helpers/usersession');
 
 module.exports.nextClass = [
     (session, args) => {
-        if (!args.firstname) args.firstname = ""
-        api.room(session, args.firstname, args.prof, (err, results) => {
-            if (results.success) {//dagdag kapag walang time si prof
-                session.endDialog(format(consts.prompts.GET_STARTED, results.data));
-            }
-        })
-
-    }
-]
-
-module.exports.room = [
-    (session, args) => {
-        console.log(args,'args sched')
         try {
             console.log('went down here')
-            api.room(session, args.firstname, args.prof, (err, results) => {
+            api.nextClass(session, args.firstname, args.prof, (err, results) => {
                 console.log(results, "asd")
-                // if (results.success) {
-                    //dagdag kapag walang time si prof
-                    session.endDialog(format(consts.prompts.PROF_ROOM, "at " + results.data));
-                // }
+                if (results.success) {
+                //dagdag kapag walang time si prof
+                session.endDialog(format(consts.prompts.PROF_ROOM, "at " + results.data));
+                }
             })
-        }catch (exception) {
+        } catch (exception) {
             api.room(session, "", args.prof, (err, results) => {
                 if (results.success) {//dagdag kapag walang time si prof
                     console.log(results, "catch")
@@ -39,6 +26,29 @@ module.exports.room = [
                 }
             })
         }
-        
+    }
+]
+
+module.exports.room = [
+    (session, args) => {
+        console.log(args, 'args sched')
+        try {
+            console.log('went down here')
+            api.room(session, args.firstname, args.prof, (err, results) => {
+                console.log(results, "asd")
+                if (results.success) {
+                //dagdag kapag walang time si prof
+                session.endDialog(format(consts.prompts.PROF_ROOM, "at " + results.data));
+                }
+            })
+        } catch (exception) {
+            api.room(session, "", args.prof, (err, results) => {
+                if (results.success) {//dagdag kapag walang time si prof
+                    console.log(results, "catch")
+                    session.endDialog(format(consts.prompts.PROF_ROOM, "at " + results.data));
+                }
+            })
+        }
+
     }
 ]
