097fc87b3bb30f0aa4db64438e68125736bbd775
RomeDorado <romedorado@gmail.com>
2018-03-08 08:44:30 +0800

    dep

--- src/dialogs/replies.js
+++ src/dialogs/replies.js
@@ -7,39 +7,54 @@ const request = require('request');
 
 module.exports = [
     (session, args) => {
-        
-        var options = { method: 'GET',
-        url: 'http://iics-response-service.herokuapp.com/api/response/intents',
-        qs: { 
-            client: 'iics', 
-            intent: args
-        },
-        headers: { 
-         'Content-type': 'Application/json', 
-         'API-Token': process.env.API_TOKEN
-        },
-        json: true
-    };
-      
-      request(options, function (error, response, body) {
-        if (error) throw new Error(error);
-        var replies = response.body.data;
-        console.log(replies)
-        var random = replies[Math.floor(Math.random() * replies.length)];
-        if(random && random.includes("cloudinary")){
-            var img = new builder.Message(session)
+
+        var options = {
+            method: 'GET',
+            url: 'http://iics-response-service.herokuapp.com/api/response/intents',
+            qs: {
+                client: 'iics',
+                intent: args
+            },
+            headers: {
+                'Content-type': 'Application/json',
+                'API-Token': process.env.API_TOKEN
+            },
+            json: true
+        };
+
+        request(options, function (error, response, body) {
+            if (error) throw new Error(error);
+            var replies = response.body.data;
+            console.log(replies, "reply")
+
+            replies.forEach(async function (element) {
+                if (element.includes("cloudinary")) {
+                    var img = new builder.Message(session)
                         .addAttachment({
-                            contentURL: random,
-                            contentType: 'image/jpg',                            
-                        });                    
-                    session.endDialog(img);
-        }else{
-            session.endDialog(random);
-        }
+                            contentURL: element,
+                            contentType: 'image/jpg',
+                        });
+                    session.send(img);
+                }else{
+                    session.send(element)                    
+                }
+            })
+
+            // var random = replies[Math.floor(Math.random() * replies.length)];
+            // if (random && random.includes("cloudinary")) {
+            //     var img = new builder.Message(session)
+            //         .addAttachment({
+            //             contentURL: random,
+            //             contentType: 'image/jpg',
+            //         });
+            //     session.endDialog(img);
+            // } else {
+            //     session.endDialog(random);
+            // }
+
+
+        });
         
-
-      });
-
-      
+session.endConversation();
     }
 ]
