d0827b7ad841abdd0ff18bdcecb707e4ca274ed4
RomeDorado <romedorado@gmail.com>
2018-03-14 23:14:43 +0800

    asd

--- src/dialogs/announcements.js
+++ src/dialogs/announcements.js
@@ -81,7 +81,7 @@ module.exports.department = [
 
                         var index = 0;
                         // if (typeof body.d[index] == 'undefined'){
-                        if (!body.d[0]) {
+                        if (typeof body.d[0] == 'undefined' || !body.d[0]) {
                             console.log("null ang body")
                             session.endConversation(consts.prompts.NO_DEPARTMENT_ANNOUNCEMENTS);
 
