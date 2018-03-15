b3a0b21edc388e72074bb7382d75fd4512c874e7
RomeDorado <romedorado@gmail.com>
2018-03-15 13:41:02 +0800

    asd

--- src/helpers/apiRequest.js
+++ src/helpers/apiRequest.js
@@ -246,7 +246,7 @@ module.exports.nextClass =
         var options = {
             method: 'GET',
             // url: 'https://iics-schedule-service.herokuapp.com/api/getNextClass',
-            url: `${process.env.SCHEDULE.API}/getNextClass`,
+            url: `${process.env.SCHEDULE_API}/getNextClass`,
             headers:
                 {
                     'API-Token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdXRoIjoiSGpaTGVJYlRtRlpzNlRTR0lDUE1lNU9FdVA0OFJDSXIiLCJkYXRhIjp7Il9pZCI6IjVhNTA4YWE5NzYxYTY3MDAxNDA1ODliNCIsInVzZXJuYW1lIjoiaWljc19hZG1pbjEyMyIsImVtYWlsIjoic2FtcGxlIiwiaGFwcHkiOjIwLCJzYWQiOjMsInBlcm1pc3Npb24iOiJBZG1pbiIsIm5hbWUiOnsiZmlyc3QiOiJJSUNTIiwibGFzdCI6IkFkbWluIn19LCJleHBpcmVJbiI6IjI0aCIsImlhdCI6MTUyMDE0OTg3MH0.A8jv7Gbqe61vfF3e1rdzrONbN0arB8vAEaNA6w509h8',
@@ -278,7 +278,7 @@ module.exports.room =
         var options = {
             method: 'GET',
             // url: 'https://iics-schedule-service.herokuapp.com/api/getRoom',
-            url: `${process.env.SCHEDULE.API}/api/getRoom`,
+            url: `${process.env.SCHEDULE_API}/api/getRoom`,
             headers:
                 {
                     'API-Token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdXRoIjoiSGpaTGVJYlRtRlpzNlRTR0lDUE1lNU9FdVA0OFJDSXIiLCJkYXRhIjp7Il9pZCI6IjVhNTA4YWE5NzYxYTY3MDAxNDA1ODliNCIsInVzZXJuYW1lIjoiaWljc19hZG1pbjEyMyIsImVtYWlsIjoic2FtcGxlIiwiaGFwcHkiOjIwLCJzYWQiOjMsInBlcm1pc3Npb24iOiJBZG1pbiIsIm5hbWUiOnsiZmlyc3QiOiJJSUNTIiwibGFzdCI6IkFkbWluIn19LCJleHBpcmVJbiI6IjI0aCIsImlhdCI6MTUyMDE0OTg3MH0.A8jv7Gbqe61vfF3e1rdzrONbN0arB8vAEaNA6w509h8',
@@ -310,7 +310,7 @@ module.exports.currentClass =
         var options = {
             method: 'GET',
             // url: 'https://iics-schedule-service.herokuapp.com/api/getRoom',
-            url: `${process.env.SCHEDULE.API}/api/getCurrentClass`,
+            url: `${process.env.SCHEDULE_API}/api/getCurrentClass`,
             headers:
                 {
                     'API-Token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdXRoIjoiSGpaTGVJYlRtRlpzNlRTR0lDUE1lNU9FdVA0OFJDSXIiLCJkYXRhIjp7Il9pZCI6IjVhNTA4YWE5NzYxYTY3MDAxNDA1ODliNCIsInVzZXJuYW1lIjoiaWljc19hZG1pbjEyMyIsImVtYWlsIjoic2FtcGxlIiwiaGFwcHkiOjIwLCJzYWQiOjMsInBlcm1pc3Npb24iOiJBZG1pbiIsIm5hbWUiOnsiZmlyc3QiOiJJSUNTIiwibGFzdCI6IkFkbWluIn19LCJleHBpcmVJbiI6IjI0aCIsImlhdCI6MTUyMDE0OTg3MH0.A8jv7Gbqe61vfF3e1rdzrONbN0arB8vAEaNA6w509h8',
@@ -342,7 +342,7 @@ module.exports.subjectTime =
         var options = {
             method: 'GET',
             // url: 'https://iics-schedule-service.herokuapp.com/api/getRoom',
-            url: `${process.env.SCHEDULE.API}/api/getSubjectTime`,
+            url: `${process.env.SCHEDULE_API}/api/getSubjectTime`,
             headers:
                 {
                     'API-Token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdXRoIjoiSGpaTGVJYlRtRlpzNlRTR0lDUE1lNU9FdVA0OFJDSXIiLCJkYXRhIjp7Il9pZCI6IjVhNTA4YWE5NzYxYTY3MDAxNDA1ODliNCIsInVzZXJuYW1lIjoiaWljc19hZG1pbjEyMyIsImVtYWlsIjoic2FtcGxlIiwiaGFwcHkiOjIwLCJzYWQiOjMsInBlcm1pc3Npb24iOiJBZG1pbiIsIm5hbWUiOnsiZmlyc3QiOiJJSUNTIiwibGFzdCI6IkFkbWluIn19LCJleHBpcmVJbiI6IjI0aCIsImlhdCI6MTUyMDE0OTg3MH0.A8jv7Gbqe61vfF3e1rdzrONbN0arB8vAEaNA6w509h8',
@@ -378,7 +378,7 @@ module.exports.subjectDay =
         var options = {
             method: 'GET',
             // url: 'https://iics-schedule-service.herokuapp.com/api/getRoom',
-            url: `${process.env.SCHEDULE.API}/api/getSubjectTimeWhen`,
+            url: `${process.env.SCHEDULE_API}/api/getSubjectTimeWhen`,
             headers:
                 {
                     'API-Token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdXRoIjoiSGpaTGVJYlRtRlpzNlRTR0lDUE1lNU9FdVA0OFJDSXIiLCJkYXRhIjp7Il9pZCI6IjVhNTA4YWE5NzYxYTY3MDAxNDA1ODliNCIsInVzZXJuYW1lIjoiaWljc19hZG1pbjEyMyIsImVtYWlsIjoic2FtcGxlIiwiaGFwcHkiOjIwLCJzYWQiOjMsInBlcm1pc3Npb24iOiJBZG1pbiIsIm5hbWUiOnsiZmlyc3QiOiJJSUNTIiwibGFzdCI6IkFkbWluIn19LCJleHBpcmVJbiI6IjI0aCIsImlhdCI6MTUyMDE0OTg3MH0.A8jv7Gbqe61vfF3e1rdzrONbN0arB8vAEaNA6w509h8',
